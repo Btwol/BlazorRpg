86b79f1da9be82df5bdda661122348dff6dc5d4c
bw <bartwol111@gmail.com>
2022-05-22 16:25:48 +0200

    Base Project Template

--- BlazorRpg/Client/ClientServices/BaseClientService/BaseClientService.cs
+++ BlazorRpg/Client/ClientServices/BaseClientService/BaseClientService.cs
@@ -1,5 +1,5 @@
 ﻿//using System.Net.Http.Json;
-
+//
 //namespace BlazorRpg.Client.ClientServices.BaseClientService
 //{
 //    public class BaseClientService<T> : IBaseClientService<T> where T : IBaseModel
